46e84d8571af4bb5fbf80cdc38e2d569765cbcff
ashumsd7 <40313523+ashumsd7@users.noreply.github.com>
2020-06-08 12:53:29 +0530

    DailyLearning_home_page

---
+++ app.js
@@ -0,0 +1,19 @@
+new Vue({
+    el: "#app",
+  
+    data() {
+        return {
+           time: "dd" 
+        }
+    },
+    methods: {
+        nounClicked(){
+            alert("a")
+        },
+      
+    },
+
+   ,
+   
+
+})
